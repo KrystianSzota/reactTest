a708b15c16f4d51f518ff7e1f820528d9e647a69
szotas <krystian.szota.it@gmail.com>
2023-01-31 20:58:39 +0100

    seconds and decySecond add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      seconds: 0,
+      decySeconds: 0
+    };
+
+    setInterval(() => { this.tick() }, 100);
+  }
+
+  tick() {
+    this.setState((state, props) => {
+      let decySeconds = state.decySeconds + 1;
+      let seconds = state.seconds;
+
+      if(decySeconds === 10){
+        seconds++;
+        decySeconds = 0;
+      }
+
+      return {
+        decySeconds: decySeconds,
+        seconds: seconds
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div>{this.state.seconds} : {this.state.decySeconds}</div>
+    );
+  }
 }
 
 export default App;
